29f3a985791831a26fcce6b9f47ea1489093b2e2
Audric Kim <audrickim@gmail.com>
2012-11-05 12:06:36 -0500

    basic calc 1 and 2

---
+++ labs/basic_calculator_v2.rb
@@ -0,0 +1,33 @@
+
+# this is a comment
+
+puts "Do you want to (a)dd, (s)ubtract, (m)ultiply, (d)ivide or (q)uit?"
+response = gets.chomp
+while response != 'q'
+	puts "First?"
+	first = gets.to_f
+	puts = "Second?"
+	second = gets.to_f
+
+	if response == 'a'
+		total = first + second
+	else response == 's'
+		total = first - second
+	end
+
+	if response == 'm'
+		total = first * second
+	else response == 'd'
+		total = first / second 
+	end
+
+	puts "The result of #{first} and #{second} is #{total}"
+	puts "Do you want to (a)dd, (s)ubtract (m)ultiply, (d)ivide or (q)uit?"
+	response = gets.chomp
+
+end
+
+
+
+
+
