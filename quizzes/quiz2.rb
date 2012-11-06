6cf2526f725730582e3598c327023dc921bb9721
Audric Kim <audrickim@gmail.com>
2012-11-06 11:19:51 -0500

    quiz2

---
+++ quizzes/quiz2.rb
@@ -0,0 +1,21 @@
+require 'pry'
+
+(250..180000).each do |x|
+	puts "the sum of the numbers is #{x + x}"
+
+end
+
+
+	puts "(i)nput a color or (q)uit"
+	response = gets.chomp
+
+	# if user responds with q, show all color inputs
+	while response != 'q'
+		if ('q') 
+
+	colors = []
+	puts colors (0)
+
+
+end
+
