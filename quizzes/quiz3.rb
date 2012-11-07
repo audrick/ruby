dffeae7e3fcefc9e08535c102eaf1394b74ad64e
Audric Kim <audrickim@gmail.com>
2012-11-07 17:08:34 -0500

    quiz3

--- quizzes/quiz3.rb
+++ quizzes/quiz3.rb
@@ -34,44 +34,44 @@ class Student
 end
 
 # hard coding students into code
-# s1 = Student.new('phil', 'january', 'male', '4.0', 'biology')
+s1 = Student.new('phil', 'january', 'male', '4.0', 'biology')
 
-# s2 = Student.new('tom', 'february', 'male', '3.0', 'chemistry')
+s2 = Student.new('tom', 'february', 'male', '3.0', 'chemistry')
 
-# s3 = Student.new('mike', 'march', 'male', '3.1', 'law')
+s3 = Student.new('mike', 'march', 'male', '3.1', 'law')
 
 
-# students << s1 << s2 << s3
+students << s1 << s2 << s3
 
-# students.each do |student|
-# 	puts student	
-# end
+students.each do |student|
+	puts student	
+end
 
 
 
 # creating students using input
-puts "Do you want to add a student? (y)es or (n)o"
-response = gets.chomp
+# puts "Do you want to add a student? (y)es or (n)o"
+# response = gets.chomp
 	
-while response != 'n'
-	puts "Name"
-	name = gets.chomp
-	puts "D.O.B?"
-	dob = gets.chomp
-	puts "Gender?"
-	gender = gets.chomp
-	puts "GPA?"
-	gpa = gets.chomp
-	puts "Major?"
-	major = gets.chomp
+# while response != 'n'
+# 	puts "Name"
+# 	name = gets.chomp
+# 	puts "D.O.B?"
+# 	dob = gets.chomp
+# 	puts "Gender?"
+# 	gender = gets.chomp
+# 	puts "GPA?"
+# 	gpa = gets.chomp
+# 	puts "Major?"
+# 	major = gets.chomp
 
-	s1 = Student.new(name, dob, gender, gpa, major)
+# 	s1 = Student.new(name, dob, gender, gpa, major)
 
-	students << s1
+# 	students << s1
 
-	puts "Do you want to add a student? (y)es or (n)o"
-	response = gets.chomp
-end
+# 	puts "Do you want to add a student? (y)es or (n)o"
+# 	response = gets.chomp
+# end
 
 puts students
 
